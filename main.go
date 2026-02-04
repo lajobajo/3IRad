1b9cd23b57d2a42e611ba300b232b9a3d86064c7
lajobajo <leopattersonleo@gmail.com>
2026-02-04 18:55:25 +0100

    made it possible to add multiple x's

--- main.go
+++ main.go
@@ -6,25 +6,41 @@ import (
 
 func main() {
 	var input string
+	var counter int
+	var cords []string
 
-	fmt.Scan(&input)
+	for true {
+		fmt.Scan(&input)
 
-	var letter byte = input[0]
-	var number byte = input[1]
+		cords = append(cords, input)
 
-	var x_pick int = int(number-'0') - 1
-	var y_pick int = int(letter - 'a')
+		for y := 0; y < 3; y++ {
+			for x := 0; x < 3; x++ {
+				var fill bool = false
 
-	for y := 0; y < 3; y++ {
-		for x := 0; x < 3; x++ {
-			if y == y_pick && x == x_pick {
-				fmt.Print("x ")
-			} else {
-				fmt.Print("o ")
+				for _, cord := range cords {
+					var letter byte = cord[0]
+					var number byte = cord[1]
+
+					var x_pick int = int(number-'0') - 1
+					var y_pick int = int(letter - 'a')
+
+					if y == y_pick && x == x_pick {
+						fill = true
+						break
+					}
+				}
+
+				if fill {
+					fmt.Print("x ")
+				} else {
+					fmt.Print("o ")
+				}
 			}
 
+			fmt.Print("\n")
 		}
 
-		fmt.Print("\n")
+		counter += 1
 	}
 }
